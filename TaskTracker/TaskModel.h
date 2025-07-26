2815fe3c3981bc1902e62db29c21f55107cf40da
Dubrovskirill <zet10set11@gmail.com>
2025-07-26 14:48:01 +0700

    Добавлен заголовочный файл TaskModel для управления списком задач и интеграции с QML

---
+++ TaskTracker/TaskModel.h
@@ -0,0 +1,46 @@
+#ifndef TASKMODEL_H
+#define TASKMODEL_H
+
+#include <QAbstractListModel>
+#include <QList>
+#include "task.h"
+
+
+class TaskModel :public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    explicit TaskModel(QObject *parent = nullptr);
+    ~TaskModel();
+
+    enum TaskRoles {
+        IdRole = Qt::UserRole + 1,
+        NameRole,
+        DescriptionRole,
+        DueDateRole,
+        IsCompletedRole,
+        PriorityRole,
+        CreatedAtRole,
+        UpdatedAtRole
+    };
+    Q_ENUM(TaskRoles)
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+
+    Q_INVOKABLE void addTask(const Task &task);
+    Q_INVOKABLE void removeTask(int row);
+    Q_INVOKABLE Task* getTask(int row) const;
+    Q_INVOKABLE void clear();
+
+
+    QList<Task*> tasks() const;
+
+private:
+    QList<Task*> m_tasks;
+
+};
+
+#endif // TASKMODEL_H
